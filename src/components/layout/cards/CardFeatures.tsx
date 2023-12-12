86d883983863c17ff5d1321579e6f2e6150a2f0e
TerratekMusic <35459364+TerratekMusic@users.noreply.github.com>
2023-12-12 14:11:43 -0600

    card features

--- src/components/layout/cards/CardFeatures.tsx
+++ src/components/layout/cards/CardFeatures.tsx
@@ -3,7 +3,7 @@
 import { Box, Heading, Text } from "@chakra-ui/react";
 import styles from "./Card.module.scss";
 
-function CardFeatures(props: { title: string }) {
+function CardFeatures(props: { title: string; description: string }) {
   return (
     <Box
       m={{ base: ".5rem", sm: "1rem", md: "2rem" }}
@@ -16,9 +16,7 @@ function CardFeatures(props: { title: string }) {
         {props.title}
       </Heading>
       <Text fontSize={{ sm: "1rem", md: ".8rem" }} color="white" m="1rem">
-        Leverage your synthetic assets as collateral for stablecoin borrowing.
-        Our streamlined platform offers secure, transparent lending, giving you
-        the flexibility to enhance your investment strategies effectively.
+        {props.description}
       </Text>
     </Box>
   );
